0c286d09931c8f67f13ab01d05975dbc708080eb
Hamilton Gabriel <gabrielzinho.gabriel2@gmail.com>
2020-05-06 23:05:32 -0400

    add components dynamics

--- src/components/FormWizard.vue
+++ src/components/FormWizard.vue
@@ -1,10 +1,6 @@
 <template>
   <div>
-    <FormPlanPicker v-if="currentStepNumber === 1" @update="processStep" />
-    <FormUserDetails v-if="currentStepNumber === 2" @update="processStep" />
-    <FormAddress v-if="currentStepNumber === 3" @update="processStep" :wizard-data="form" />
-    <FormReviewOrder v-if="currentStepNumber === 4" @update="processStep" :wizard-data="form" />
-
+    <component :is="currentStep" @update="processStep" :wizard-data="form"></component>
     <div class="progress-bar">
       <div :style="`width: ${progress}%;`"></div>
     </div>
@@ -33,8 +29,13 @@ export default {
   data() {
     return {
       currentStepNumber: 1,
-      length: 4,
       canGoNext: false,
+      steps: [
+        "FormPlanPicker",
+        "FormUserDetails",
+        "FormAddress",
+        "FormReviewOrder"
+      ],
       form: {
         plan: null,
         email: null,
@@ -48,6 +49,12 @@ export default {
     };
   },
   computed: {
+    length() {
+      return this.steps.length;
+    },
+    currentStep() {
+      return this.steps[this.currentStepNumber - 1];
+    },
     progress() {
       return (this.currentStepNumber / this.length) * 100;
     }
